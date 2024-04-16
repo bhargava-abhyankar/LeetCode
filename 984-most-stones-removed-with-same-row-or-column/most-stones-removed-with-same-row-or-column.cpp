bd4f00c4ef0a199a92be4956bb8c26c69b4fe110
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-04-16 12:27:03 +0530

    Time: 58 ms (39.84%) | Memory: 17.8 MB (81.50%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /*  This method is using DFS 
+
     void dfs(int i, vector<bool> &is_vis, vector<vector<int>>& stones)
     {
         is_vis[i] = true;
@@ -30,5 +32,70 @@ public:
         }
 
         return (n - count);    
+    }
+    */
+
+    /* This method is using disjoint/ find and union */
+
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x)
+    {
+        if(parent[x] == x)
+            return x;
+        
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y)
+    {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent) {
+            return;
+        }
+
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        }
+        else if (rank[x_parent] < rank[y_parent])
+        {
+            parent[x_parent] = y_parent;
+        }
+        else
+        {
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
+    }
+
+    int removeStones(vector<vector<int>>& stones) 
+    {
+        int n = stones.size();
+        rank.resize(n);
+        parent.resize(n);
+
+        for(int i = 0; i < n; i++) {
+            rank[i] = 1;
+            parent[i] = i;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i +1; j < n; j++) {
+                if(stones[j][0] == stones[i][0] || stones[j][1] == stones[i][1])
+                    Union(i, j);
+            }
+        }
+
+        int groups = 0;
+        for(int i = 0; i < n; i++) {
+            if(find(i) == i) {
+                groups++;
+            }
+        }
+
+        return n - groups;
     }
 };
