1d3dfcc04c3f3db75f1b4dc7d0143690536cc0ca
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-04-19 15:52:11 +0530

    Time: 271 ms (93.88%) | Memory: 123.7 MB (91.25%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
 
+    /* DFS method */
+
+    /*
     void dfs(vector<vector<int>> &adj, int i, int destination, vector<bool> &is_vis, bool &ans)
     {
         is_vis[i] = true;
@@ -33,4 +36,62 @@ public:
         dfs(adj, source, destination, is_vis, ans);
         return ans;
     }
+    */
+
+    /* Union Find Method */
+
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x)
+    {
+        if(parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y)
+    {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent) {
+            return;
+        }
+
+        if(x_parent > y_parent) {
+            parent[y_parent] = x_parent;
+        }
+        else if (x_parent < y_parent) {
+            parent[x_parent] = y_parent;
+        }
+        else {
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
+    }
+
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) 
+    {
+        parent.resize(n);
+        rank.resize(n);
+
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        for(int i = 0; i < edges.size(); i++) {
+            Union(edges[i][0], edges[i][1]);
+        }
+
+        if(find(source) == find(destination)) {
+            return true;
+        }
+
+        return false;
+    }
+
 };
