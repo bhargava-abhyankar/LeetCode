086c0f4577588c8e44ade62eb72d2117c53f4c09
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-05-05 17:03:09 +0530

    Time: 118 ms (49.27%) | Memory: 18.4 MB (84.13%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.cpp
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.cpp
@@ -15,6 +15,9 @@ public:
         }
     }
 
+    /* With simple way of building the graph */
+
+    /*
     int maximumDetonation(vector<vector<int>>& bombs) 
     {
         int result = 0;
@@ -32,6 +35,7 @@ public:
                 int x1 = bombs[i][0];
                 int y1 = bombs[i][1];
                 long long r1 = bombs[i][2];
+
                 int x2 = bombs[j][0];
                 int y2 = bombs[j][1];
                 long long r2 = bombs[j][2];
@@ -40,6 +44,48 @@ public:
 
                 if( dist <= ((long long) r1 * r1)) {
                     adj[i].push_back(j);
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            count = 0;
+            is_vis = vector(n, false);
+            dfs(adj, i , is_vis, count);
+            result = max(result, count);
+        }
+        
+        return result;
+    }
+    */
+
+    int maximumDetonation(vector<vector<int>>& bombs) 
+    {
+        int result = 0;
+        int count = 0;
+        int n = bombs.size();
+        vector<vector<int>> adj(n);
+        vector<bool> is_vis(n, false);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++ ) {
+
+                int x1 = bombs[i][0];
+                int y1 = bombs[i][1];
+                long long r1 = bombs[i][2];
+
+                int x2 = bombs[j][0];
+                int y2 = bombs[j][1];
+                long long r2 = bombs[j][2];
+
+                long long dist = (pow((x2-x1), 2) + pow((y2-y1), 2));
+
+                if( dist <= ((long long) r1 * r1)) {
+                    adj[i].push_back(j);
+                }
+
+                if( dist <= ((long long) r2 * r2)) {
+                    adj[j].push_back(i);
                 }
             }
         }
