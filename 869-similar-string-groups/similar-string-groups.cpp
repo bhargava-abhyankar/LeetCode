df0d71783b0c6d57b6218dbcc25fa01d511b898e
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-05-12 17:44:00 +0530

    Time: 192 ms (26.81%) | Memory: 112.8 MB (27.49%) - LeetSync

--- 869-similar-string-groups/similar-string-groups.cpp
+++ 869-similar-string-groups/similar-string-groups.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    /* DFS method */
+    /* common function */
 
     bool is_similar(string str1, string str2)
     {
@@ -9,17 +9,19 @@ public:
         int diff = 0;
 
         /* here we need to determine only how many places the strings are different, no need to swap and check if they are equal. if the 
-        *  diff is greater than 2 then false */
-        
+         * diff is greater than 2 then false */
+
         for(int i = 0; i < n; i++) {
             if(str1[i] != str2[i])
                 diff++;
         }
 
         return( diff == 0 || diff == 2);
-
     }
 
+    /* DFS method */
+
+    /*
     void dfs(vector<vector<int>> &adj, int i, vector<bool> &is_vis)
     {
         is_vis[i] = true;
@@ -56,5 +58,58 @@ public:
             }
         }
         return count;
+    }
+    */
+
+    /* Union find method */
+
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x)
+    {
+        if(x == parent[x]) {
+            return x;
+        }
+
+        return (parent[x] = find(parent[x]));
+    }
+
+    void Union(int x, int y)
+    {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } 
+        else if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[y_parent] = x_parent;
+        }
+    }
+
+    int numSimilarGroups(vector<string>& strs) 
+    {
+        int n = strs.size();
+        int groups = n;
+
+        rank.resize(n, 1);
+        parent.resize(n, 0);
+
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(is_similar(strs[i], strs[j]) && find(i) != find(j)) {
+                    Union(i, j);
+                    groups--;
+                }     
+            }
+        }
+        return groups;
     }
 };
