2e725abf74daecde722c5ae45b96c8dd66a5b04c
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-05-18 10:54:58 +0530

    Time: 94 ms (97.48%) | Memory: 50.8 MB (95.46%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
 
+    /*
     int is_cycle(vector<vector<int>>& graph, int i, vector<bool> &is_vis, vector<bool> &cur_stack)
     {
         is_vis[i] = true;
@@ -41,4 +42,40 @@ public:
 
         return result;
     }
+    */
+
+    bool dfs(vector<vector<int>>& graph, int node, vector<int>& state) 
+    {
+        /* If the node is already visited, check if it's safe */
+        
+        if (state[node] == 1) 
+            return true;
+
+        /* If the node is currently being visited, it's part of a cycle, not safe */
+
+        if (state[node] == 2) 
+            return false;
+    
+        state[node] = 2; // Mark node as being visited
+    
+        for (int neighbor : graph[node]) {
+            if (!dfs(graph, neighbor, state)) return false; // If any neighbor is not safe, the node is not safe
+        }
+    
+        state[node] = 1; // Mark node as safe
+        return true;
+    }
+
+vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    int n = graph.size();
+    vector<int> state(n, 0); // 0: not visited, 1: safe, 2: being visited
+    
+    vector<int> result;
+    for (int i = 0; i < n; ++i) {
+        if (dfs(graph, i, state)) result.push_back(i);
+    }
+    
+    return result;
+}
+
 };
