6b2be277c01d8ea4fd0b6981518099b070a89158
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-05-19 19:48:59 +0530

    Time: 432 ms (24.00%) | Memory: 18.5 MB (52.21%) - LeetSync

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
 
+    /*BFS method.
+     find the minimum dist of each node to all other nodes using bfs and count them 
+     */
+
+    /*
     void bfs(int source, vector<vector<int>> &adj, int &n, vector<int> &dist)
     {
         queue<pair<int, int>> q;
@@ -55,4 +60,44 @@ public:
 
         return ans;      
     }
+    */
+
+    /* Floyd marshal */
+
+    vector<int> countOfPairs(int n, int x, int y) 
+    {
+        vector<vector<int>> matrix(n+1, vector<int>(n+1, 100000));
+
+        for(int i = 1; i <n; i++) {
+            matrix[i][i+1] = 1;
+            matrix[i+1][i] = 1;
+            matrix[i][i] = 0;
+        }
+
+        matrix[x][y] = 1;
+        matrix[y][x] = 1;
+        
+        for(int via = 1; via <=n; via++) {
+            for(int i = 1; i <=n; i++) {
+                for(int j = 1; j <=n; j++) {
+                    matrix[i][j] =  min(matrix[i][j], matrix[i][via] + matrix[via][j]);
+                }
+            }
+        }
+     
+        vector<int> ans(n, 0);
+
+        for(int i = 1; i <=n; i++) {
+            for(int j = 1; j <=n; j++) {
+
+                if(i != j) {
+                    int val = matrix[i][j];
+                    ans[val-1]++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
 };
