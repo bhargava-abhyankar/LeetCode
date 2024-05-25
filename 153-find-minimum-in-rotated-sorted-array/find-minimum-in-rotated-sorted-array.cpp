cbbddb185fbbf11547b1669e546f909c9ca09efc
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-05-26 01:47:45 +0530

    Time: 3 ms (56.98%) | Memory: 12.5 MB (58.62%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -44,11 +44,16 @@ public:
                 start = mid + 1;
             } else {
                 ans = min(ans, nums[mid]);
-                end = mid;
+                end = mid - 1;
             }
         }
 
         return ans;
     }
+
+    /* Important Note :
+       Q. Find how many times the sorted array has been rotated.
+       A. Find the min element in sorted rotated array. Its index will be number of times array has been rotated 
+     */
 
 };
