23c5cfb973b4c2f1be667b7ffc1c352135b2524e
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-05-25 16:20:15 +0530

    Time: 3 ms (89.09%) | Memory: 16.1 MB (11.59%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
 
-    /* method 1 */
+    /* method 1 : lower bound and upper bound approach */
 
+    /*
     int lower_bound(vector<int>& nums, int target)
     {
         int ans = nums.size();
@@ -56,6 +57,62 @@ public:
             ans[0] = low_bound;
             ans[1] = up_bound-1;
         }
+
+        return ans;
+    }
+
+    */
+
+    /*************************************************************************************************************************/
+    
+    /* Method 2: modified binary search to calculate first and last occurence */
+
+    int first_occurence_binary_search(vector<int>& nums, int target)
+    {
+        int ans = -1;
+        int start = 0, end = nums.size()-1;
+
+        while(start <= end) {
+            int mid = (start + end)/2;
+
+            if(nums[mid] == target) {
+                ans = mid;
+                end = mid -1;
+            } else if (nums[mid] > target) {
+                end = mid -1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    int last_occurence_binary_search(vector<int>& nums, int target)
+    {
+        int ans = -1;
+        int start = 0, end = nums.size()-1;
+
+        while(start <= end) {
+            int mid = (start + end)/2;
+
+            if(nums[mid] == target) {
+                ans = mid;
+                start = mid +1;
+            } else if (nums[mid] > target) {
+                end = mid -1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) 
+    {
+        vector<int> ans(2, -1);
+
+        ans[0] = first_occurence_binary_search(nums, target);
+        ans[1] = last_occurence_binary_search(nums, target);
 
         return ans;
     }
