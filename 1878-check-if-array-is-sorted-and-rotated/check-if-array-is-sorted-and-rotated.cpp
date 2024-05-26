5e79f86fa597cd00484b157d76406482edbd8ec1
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-05-26 20:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (90.55%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -13,10 +13,19 @@ public:
             }
         }
 
-        if(count != 0 && nums[n] > nums[0])
+        if(count != 0 && nums[n] > nums[0]) /* condtion not required if no rotation found. i.e array is fully sorted */
             return false;
 
         return true;
     }
+
+    /* Explanation :
+       The solution will be O(n) only. 
+       
+       If the array is sorted, or sorted and rotated, at max you will find one descrepency in ascending order. 
+       Count the descrepency by looping and if it exceeds 1, its not sorted rotated. Also if it rotated, then
+       last element in the array should be less than the first element in the array. i.e after first descrepency
+       all the elements should be smaller than the smallest element (first element) in first half. 
+    */
 
 };
