7e7e4edfc72674491a235dc011908632093b172a
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-10 16:44:29 +0530

    Time: 61 ms (44.36%) | Memory: 40.9 MB (47.79%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
 
+    /*method 1*/
+
+    /*
     long long find_cost(vector<int>& nums, vector<int>& cost, long long int cur)
     {
         long long count = 0;
@@ -35,4 +38,47 @@ public:
 
         return ans;
     }
+    */
+
+    /*method 2*/
+
+    long long find_cost(vector<int>& nums, vector<int>& cost, long long cur)
+    {
+        long long count = 0L;
+
+        for(int i = 0; i < nums.size(); i++) {
+            count = count + ((long long)(abs(nums[i] - cur)* cost[i]));
+        }
+
+        return count;
+    }
+
+    long long minCost(vector<int>& nums, vector<int>& cost) 
+    {
+        long long ans;
+
+        int start = 1, end;
+
+        for(int i = 0; i < nums.size(); i++) {
+            end = max(end, nums[i]);
+        }
+
+        while(start <= end) {
+            long long mid = (start + end) / 2;
+
+            long long mid_cost = find_cost(nums, cost, mid);
+            long long mid_cost_plus = find_cost(nums, cost, mid + 1);
+
+            ans =  min(mid_cost, mid_cost_plus);
+
+            if(mid_cost <= mid_cost_plus) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
 };
