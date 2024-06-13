0028fbe99326aa97040cda49c9af61d329e8625d
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-13 18:02:46 +0530

    Time: 3 ms (42.59%) | Memory: 7.4 MB (95.13%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -28,27 +28,57 @@ public:
 
     */
 
-    int strStr(string haystack, string needle) {
-        int hLen = haystack.length();
-        int nLen = needle.length();
-        int nIndex = 0;
-        for(int i=0; i<hLen; i++){
-            // as long as the characters are equal, increment needleIndex
-            if(haystack[i]==needle[nIndex]){
-                nIndex++;
-            }
-            else{
-                // start from the next index of previous start index
-                i=i-nIndex;
-                // needle should start from index 0
-                nIndex=0;
-            }
-            // check if needleIndex reached needle length
-            if(nIndex==nLen){
-                // return the first index
-                return i-nLen+1;
-            }
+    int strStr(string haystack, string needle) 
+    {
+        if(needle.length() > haystack.length()) {
+            return -1;
         }
+
+        int i = 0;
+
+        while(i <= (haystack.length() - needle.length())) {
+            int j = i, k = 0;
+            while( j < haystack.length() && k < needle.length() && haystack[j] == needle[k]) {
+                j++;
+                k++;
+            }
+            if(k == needle.length()) {
+                return i;
+            }
+            i++;
+        }
+
         return -1;
     }
+
+
+    /*
+    int strStr(string haystack, string needle) 
+    {
+        if(needle.length() > haystack.length()) {
+            return -1;
+        }
+
+        int i = 0, k = 0;
+
+        while(i <= (haystack.length() - needle.length())) {
+
+            if(haystack[i] == needle[k]) {
+                k++;
+            } else {
+                i = i - k;
+                k = 0;
+                
+            }
+
+            if(k == needle.length()) {
+                return (i-needle.length()+1);
+            }
+            i++;
+        }
+
+        return -1;
+    }
+    */
+
 };
