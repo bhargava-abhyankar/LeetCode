c68dcb2d4747b6816ccbbe5ac3c6a747a927b140
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-13 18:03:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (50.62%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -51,34 +51,4 @@ public:
         return -1;
     }
 
-
-    /*
-    int strStr(string haystack, string needle) 
-    {
-        if(needle.length() > haystack.length()) {
-            return -1;
-        }
-
-        int i = 0, k = 0;
-
-        while(i <= (haystack.length() - needle.length())) {
-
-            if(haystack[i] == needle[k]) {
-                k++;
-            } else {
-                i = i - k;
-                k = 0;
-                
-            }
-
-            if(k == needle.length()) {
-                return (i-needle.length()+1);
-            }
-            i++;
-        }
-
-        return -1;
-    }
-    */
-
 };
