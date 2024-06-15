04d0fb88f775d3b679308c6989f76f9ec866adab
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-15 13:49:20 +0530

    Time: 3 ms (37.51%) | Memory: 8.6 MB (17.31%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    void backtracking(map<char, string> &table, string digits, int cur_digit, string &ans, vector<string> &final_ans)
+    {
+        if(digits.length() == ans.length()) {
+            final_ans.push_back(ans);
+            return;
+        }
+
+        for(int i = cur_digit; i < digits.length(); i++) {
+            char c = digits[i];
+            string x = table[c];
+
+            for(int j = 0; j < x.length(); j++) {
+                ans.push_back(x[j]);
+                backtracking(table, digits, i+1, ans, final_ans);
+                ans.pop_back();
+            }
+        }
+    }
+
+    vector<string> letterCombinations(string digits) 
+    {
+        string cur_ans;
+        vector<string> ans;
+
+        if(digits.length() == 0) {
+            return ans;
+        }
+
+        map<char, string> table;
+        table.insert({'2', "abc"});
+        table.insert({'3', "def"});
+        table.insert({'4', "ghi"});
+        table.insert({'5', "jkl"});
+        table.insert({'6', "mno"});
+        table.insert({'7', "pqrs"});
+        table.insert({'8', "tuv"});
+        table.insert({'9', "wxyz"});
+
+        backtracking(table, digits, 0, cur_ans, ans);
+        return ans;
+    }
+};
