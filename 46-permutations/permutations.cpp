e4a834450c35e78cebb677ebbc2b5fa55ff3c06e
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-16 07:16:35 +0530

    Time: 5 ms (34.47%) | Memory: 8.7 MB (97.92%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
 
+    /*
     void compute(vector<int>&nums, int cur, vector<int>&cur_ans, vector<vector<int>>&ans, vector<bool>&is_vis)
     {
         if(cur_ans.size() == nums.size()) {
@@ -16,8 +17,7 @@ public:
                 cur_ans.pop_back();
                 is_vis[i] = false;
             }
-        }
-        
+        }    
     }
 
     vector<vector<int>> permute(vector<int>& nums) 
@@ -28,6 +28,33 @@ public:
 
         compute(nums, -1, cur_ans, ans, is_vis);
         return ans;
+    }
+    */
 
+    /*The approach is not very straight forward. For combination, based on recursive stack
+      added elements to array. This is because combination is choosing k from n. 
+      For permutation, its arranging n in different ways. ans will always have n 
+      Idea s to swap each element with itself and others */
+
+    void compute(vector<int>& nums, int cur, vector<vector<int>>&ans)
+    {
+        if(cur == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i = cur; i < nums.size(); i++) {
+            swap(nums[i], nums[cur]);
+            compute(nums, cur+1, ans);
+            swap(nums[i], nums[cur]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+        vector<vector<int>> ans;
+        compute(nums, 0, ans);
+
+        return ans;    
     }
 };
