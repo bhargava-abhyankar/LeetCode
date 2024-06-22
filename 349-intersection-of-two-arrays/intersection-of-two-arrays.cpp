c783a5b1959d1a807e2b5b504d99a232803cda3c
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-22 17:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (95.59%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -26,16 +26,17 @@ public:
     }
     */
 
+    /*
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
     {
         vector<int> ans;
         unordered_set<int> s1(nums1.begin(), nums1.end());
         unordered_set<int> s2(nums2.begin(), nums2.end());
 
-        /*Please note: here u are iterating over set, not orginial vector. Basically
-          both set will have unique numbers.  then u iterate over one set and compare it with
-          other set. if found put it to ans */
-          
+        //Please note: here u are iterating over set, not orginial vector. Basically
+        //  both set will have unique numbers.  then u iterate over one set and compare it with
+        //  other set. if found put it to ans.
+
         for(const auto &num:s1) {
             if(s2.find(num) != s2.end()) {
                 ans.push_back(num);
@@ -44,4 +45,33 @@ public:
 
         return ans;
     }
+    */
+
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+        vector<int> ans;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0, j = 0;
+
+        while(i < nums1.size() && j < nums2.size()) {
+            if(nums1[i] == nums2[j]) {
+                ans.push_back(nums1[i]);
+                while(i < nums1.size()-1 && nums1[i] == nums1[i+1])
+                    i++;
+                while(j < nums2.size()-1 && nums2[j] == nums2[j+1])
+                    j++;
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return ans;   
+    }
+
+
 };
