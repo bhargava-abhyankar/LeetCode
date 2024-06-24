692d5f7918462538aedd1e0ccb8b9692ffe4c6c6
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-24 13:00:04 +0530

    Time: 9 ms (49.13%) | Memory: 10.2 MB (43.31%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    /* Note that this is much different than the usual two pointer pattern you are using. here slow and fast both are initialized to same, since we need x and 2x speed
+       initializing them to same is nececcery. since they are initialized to same, not at 20 and 21 line we update pointer and then check. if u put it down it wont work.
+
+      Now coming to algo, once slow and fast pointer meet, change one to begining/head and then move them at same pace. they will meet at begining
+      */
+
+    ListNode *detectCycle(ListNode *head) 
+    {
+        ListNode * slow = head, *fast = head;
+
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) {
+                slow = head;
+
+                while(fast != slow) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+           
+        }
+
+        return NULL;
+    }
+};
