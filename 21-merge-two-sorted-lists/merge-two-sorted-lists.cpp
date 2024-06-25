753d6f36c3c4f2bcc2e67722b065774b413a35d7
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-25 11:49:03 +0530

    Time: 3 ms (81.91%) | Memory: 18.2 MB (75.39%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    /* This same template can be used in sorting. using merge sort. know this template very well */
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
+    {
+        ListNode *dummy = new ListNode(0);
+        ListNode *traverse = dummy;
+
+        while(list1 && list2) {
+            if(list1->val <= list2->val) {
+                traverse->next = list1;
+                list1 = list1->next;
+            } else {
+                traverse->next = list2;
+                list2 = list2->next;
+            }
+            traverse = traverse->next;
+        }
+
+        while(list1) {
+            traverse->next = list1;
+            list1 = list1->next;
+            traverse = traverse->next;
+        }
+
+        while(list2) {
+            traverse->next = list2;
+            list2 = list2->next;
+            traverse = traverse->next;
+        }
+
+        return (dummy->next);
+    }
+};
