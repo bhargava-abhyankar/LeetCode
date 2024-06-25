a8f21e9f9d09b1895d4e04a436eefa2b82491cd2
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-25 16:07:12 +0530

    Time: 7 ms (25.63%) | Memory: 12.5 MB (86.92%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    /*
     int compress(vector<char>& chars) 
     {
         int  j = 0, compressed_size = 0;
@@ -27,4 +29,40 @@ public:
 
         return compressed_size;
     }
+    */
+
+     /* Use three pointer. one to update the result after compression. for comparing if its a repeat use usual two pointer */
+
+    int compress(vector<char>& chars) 
+    {
+        int i = 0, j = 0, compressed_length = 0;
+
+        while(j < chars.size()) {
+            int count = 0;
+            i = j;
+
+            while(j < chars.size() && chars[i] == chars[j]) {
+                count++;
+                j++;
+            }
+
+            chars[compressed_length] = chars[i];
+            compressed_length++;
+
+            if(count > 1) {
+                for(char &ch: to_string(count)) {
+                    chars[compressed_length] = ch;
+                    compressed_length++;
+                }
+            }
+        }
+
+        return compressed_length;
+    }
+
+    /*use only two pointer, note how to find repeat with single pointer */
+
+
+
+
 };
