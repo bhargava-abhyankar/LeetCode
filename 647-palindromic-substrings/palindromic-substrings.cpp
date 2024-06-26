feb4facf38c82e7bd276b79f21fe0d3b03e840d1
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-06-26 13:34:13 +0530

    Time: 6 ms (79.56%) | Memory: 7.4 MB (95.38%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -3,7 +3,7 @@ public:
 
     /* Brute force: time complexity is o(n3). first two loops o(n2) and checking palindrome o(n) */
 
-
+    /*
     int is_palindrome(int i, int j, string s)
     {
         while(i <= j) {
@@ -29,5 +29,64 @@ public:
         return sum;
     }
 
-    /* This is dp problem */
+    */
+
+    /* below code is same as above code, another format in which all combination can be printed. However adding one condition to this, can make 
+       validation of palindrome optimized. while generating all combination itself we check if the addtion will lead to palindrome or not.
+       for better understanding writen below code to check if all combination are generated. further below code is writen using this template
+       for palindrome check */
+
+    /*
+    void print_combination(string s, int i, int j)
+    {
+        while(i >= 0 && j < s.length()) {
+            for(int k = i; k <=j; k++) {
+                cout << s[k];
+            }
+            i--;
+            j++;
+            cout<<endl;
+        }
+    }
+
+    int countSubstrings(string s) 
+    {
+        for(int i = 0; i < s.length(); i++) {
+            print_combination(s, i, i+1);
+            print_combination(s, i, i);    
+        }
+
+        return 0;
+    }
+
+    */
+
+
+    int is_valid_palindrome(string &s, int i, int j)
+    {
+        int count = 0;
+
+        while(i >= 0 && j < s.length() && s[i] == s[j]) {
+            i--;
+            j++;
+            count++;
+        }
+
+        return count;
+    }
+
+    int countSubstrings(string s)
+    {
+        int sum = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            int even = is_valid_palindrome(s, i, i+1);
+            int odd = is_valid_palindrome(s, i, i);
+            sum = sum + even + odd;
+        }
+
+        return sum;
+    }
+
+    /* This is actually dp problem, study dp solution, once u have started dp and do this problem */
 };
