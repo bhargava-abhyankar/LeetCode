a8949a35684ad496f63b7a042d4eac4fa5c42ddd
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-07-05 18:05:36 +0530

    Time: 19 ms (74.33%) | Memory: 20.6 MB (64.72%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+
+    /* Simple two pointer works. Because we just have to find the number of children. Also no matter what is the size of the cookie, it can be given to only 
+       one child. Just check if it satisfies the greed or not, if not go for higher number, if satisfies go for next child with next cookie.
+       Both children and cokies need to be sorted for this to work. so nlogn*/
+
     int findContentChildren(vector<int>& g, vector<int>& s) 
     {
         sort(g.begin(), g.end());
@@ -16,7 +21,7 @@ public:
                 j++;
             }
         }
-        
+
         return count;        
     }
 };
