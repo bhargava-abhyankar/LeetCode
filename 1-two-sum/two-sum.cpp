3c6f03247aa4abd815b35a25cf7b21f6056eb3cd
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-07-07 12:17:15 +0530

    Time: 12 ms (59.24%) | Memory: 14.6 MB (12.13%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,21 +1,59 @@
 class Solution {
 public:
+
+    /* Method 1: Using hash_table. time and space complexity both are O(n) and O(n) 
+
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        vector<int> ans; 
-        unordered_map<int, int> hash_table;
+        vector<int> ans;
+        unordered_map<int, int> hash;         //number and index
 
         for(int i = 0; i < nums.size(); i++) {
-            int rem = target - nums[i];
+            int t = target-nums[i];
 
-            if(hash_table.find(rem) != hash_table.end()) {
-                ans.push_back(hash_table[rem]);
+            if(hash.find(t) != hash.end()) {
                 ans.push_back(i);
+                ans.push_back(hash[t]);
                 return ans;
-            } else {
-               hash_table.insert({nums[i], i});  
-            }
 
+            } else {
+                hash.insert({nums[i], i});
+            }
+        }
+
+        return ans;
+    }
+
+    */
+
+    static bool comp(pair<int, int> &a, pair<int, int> &b)
+    {
+        return( a.first < b.first);
+    }
+
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> ans;
+        vector<pair<int, int>> num_array;
+
+        for(int i = 0; i < nums.size(); i++) {
+            num_array.push_back({nums[i], i});
+        }
+
+        sort(num_array.begin(), num_array.end(), comp);
+
+        int i = 0, j = num_array.size()-1;
+
+        while(i < j) {
+            if( (num_array[i].first + num_array[j].first) == target) {
+                ans.push_back(num_array[i].second);
+                ans.push_back(num_array[j].second);
+                return ans;
+            } else if ((num_array[i].first + num_array[j].first) > target) {
+                j--;
+            } else if ((num_array[i].first + num_array[j].first) < target) {
+                i++;
+            }
         }
 
         return ans;
