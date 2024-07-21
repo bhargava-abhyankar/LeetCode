a70097aff670a6968cf59a53a9cf6f6c2239e648
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-07-21 20:12:10 +0530

    Time: 69 ms (19.71%) | Memory: 58.1 MB (12.80%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+
+    /*Approach: Fixed window size problem. when window size reaches k, calculate avg using cur_sum and k. if avg greater the threashold,
+      count it as ans. then reduce the val at left from sum and  increment the left. */
+
     int numOfSubarrays(vector<int>& arr, int k, int threshold) 
     {
         int left = 0, right = 0, cur_sum = 0, ans = 0;
