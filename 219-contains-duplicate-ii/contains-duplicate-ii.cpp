25751da42610119ef3e9631ba562db8170ac1f8a
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-07-22 08:32:50 +0530

    Time: 107 ms (90.48%) | Memory: 76.3 MB (81.73%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+
+    /* My approach: simple hash table.
+
     bool containsNearbyDuplicate(vector<int>& nums, int k) 
     {
         unordered_map<int, int> hash;
@@ -16,7 +19,28 @@ public:
                 }
             }
         }
-        
+
+        return false;
+    }
+
+    */
+
+    /* Approach 2: hash table and sliding window */
+
+    bool containsNearbyDuplicate(vector<int>& nums, int k) 
+    {
+        unordered_set<int> hash;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(hash.find(nums[i]) != hash.end()) {
+                return true;
+            }
+            hash.insert(nums[i]);
+            if(hash.size() > k) {
+                auto itr = hash.find(nums[i-k]);
+                hash.erase(itr);
+            }
+        }
         return false;
     }
 };
