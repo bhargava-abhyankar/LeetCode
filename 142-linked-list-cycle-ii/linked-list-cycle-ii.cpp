27970ec372d0e1ad87508c90dc3b1a8ed36a40b6
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-08-16 23:33:46 +0530

    Time: 4 ms (86.26%) | Memory: 10.5 MB (28.33%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -13,7 +13,7 @@ public:
        initializing them to same is nececcery. since they are initialized to same, not at 20 and 21 line we update pointer and then check. if u put it down it wont work.
 
       Now coming to algo, once slow and fast pointer meet, change one to begining/head and then move them at same pace. they will meet at begining
-      */
+      
 
     ListNode *detectCycle(ListNode *head) 
     {
@@ -31,10 +31,43 @@ public:
                     fast = fast->next;
                 }
                 return slow;
-            }
-           
+            }    
         }
 
         return NULL;
     }
+
+    */
+
+    /* Same algo as above, but using my universal template of slow and fast pointer */
+    
+    ListNode *detectCycle(ListNode *head) 
+    {
+        if(!head)
+            return head;
+        
+        ListNode *dummy = new ListNode(1000000, head);
+        ListNode *slow = dummy, *fast = dummy;
+
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) {
+                slow = dummy;
+
+                while(slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+
+                delete dummy;
+                return slow;
+            }
+        }
+
+        delete dummy;
+        return NULL;
+    }
+
 };
