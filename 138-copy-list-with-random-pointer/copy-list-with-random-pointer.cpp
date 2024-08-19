6e4563649f0fdf97c2a4ccb2a405520721b50097
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-08-19 21:26:31 +0530

    Time: 3 ms (91.38%) | Memory: 14.7 MB (35.27%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,6 +17,8 @@ public:
 class Solution {
 public:
 
+    /*Method 1: simple two pass solution. time complexity O(n) and space complexity O(n)
+
     Node* copyRandomList(Node* head) 
     {
         unordered_map<Node*, Node*> hash;
@@ -41,5 +43,46 @@ public:
         new_list_traverse = new_list->next;
         delete(new_list);
         return new_list_traverse;
+    }
+
+    */
+
+    Node* copyRandomList(Node* head) 
+    {
+        Node *traverse = head, *new_node = NULL;
+
+        while(traverse) {
+            new_node = new Node(traverse->val);
+            new_node->next = traverse->next;
+            traverse->next = new_node;
+            traverse = new_node->next;
+        }
+
+        traverse = head;
+
+        while(traverse) {
+            new_node = traverse->next;
+            if(traverse->random)
+                new_node->random = traverse->random->next;
+            else
+                new_node->random = NULL;
+            traverse = new_node->next;
+        }
+
+        traverse = head;
+        
+        Node* new_list = new Node(10000);
+        new_node = new_list;
+        
+        while(traverse) {
+            new_node->next = traverse->next;
+            traverse->next = traverse->next->next;
+            
+            new_node = new_node->next;
+            traverse = traverse->next;
+        }
+
+        return new_list->next;
+
     }
 };
