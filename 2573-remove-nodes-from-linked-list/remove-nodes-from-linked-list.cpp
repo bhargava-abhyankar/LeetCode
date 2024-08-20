4e18dd8c1f4ecb09481cd1b6261c017d48e9958f
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-08-20 14:00:42 +0530

    Time: 349 ms (5.97%) | Memory: 191.2 MB (5.03%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -10,6 +10,43 @@
  */
 class Solution {
 public:
+    /* method 1: time O(n) and space O(n) */
+
+    ListNode* removeNodes(ListNode* head) 
+    {
+        stack<pair<ListNode*, ListNode*>> st;      // prev and cur
+        ListNode *dummy = new ListNode(-1, head);
+        ListNode *prev = dummy, *traverse = head;
+
+        while(traverse) {
+            st.push({prev, traverse});
+            prev = traverse;
+            traverse = traverse->next;
+        }
+
+        int maximum = INT_MIN;
+        
+        while(!st.empty()) {
+            auto cur = st.top().second;
+            auto previous = st.top().first;
+
+            if(cur->val < maximum) {
+                previous->next = cur->next;
+                delete cur;
+            } else {
+                maximum = max(maximum, cur->val);
+            }
+            
+            st.pop();
+        }
+
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+
+    /* method 2: time O(n) and space O(1). 3 pass solution
+
     ListNode* removeNodes(ListNode* head) 
     {
         ListNode *prev = NULL, *cur = head, *next = head;
@@ -50,4 +87,6 @@ public:
         head = prev;
         return head;
     }
+
+    */
 };
