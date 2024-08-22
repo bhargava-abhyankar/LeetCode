03e07788fc04f15568cfb6ee33d71a0a2ed2527f
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-08-22 11:47:16 +0530

    Time: 368 ms (83.84%) | Memory: 185.2 MB (53.62%) - LeetSync

---
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -0,0 +1,100 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    /*
+    ListNode* swapNodes(ListNode* head, int k) 
+    {        
+        int count = 0;
+        ListNode *dummy = new ListNode(-1, head);
+        ListNode *slow = dummy, *fast = dummy, *node_x_prev = dummy;
+
+        for(int i = 0; i <= k; i++) {
+            fast = fast->next;
+        }
+
+        while(fast) {
+            if(count == k-1) {
+                node_x_prev = slow;
+            }
+            slow = slow->next;
+            fast = fast->next;
+            count++;
+        }
+
+        ListNode *node_x = node_x_prev->next, *node_y = slow->next, *node_y_prev = slow;
+
+        cout << node_x_prev->val << ", " << node_x->val << ", " << node_y_prev->val << " , " << node_y << endl;
+
+        if(node_x == NULL || node_y == NULL)
+            return head;
+        
+        if(node_x_prev != NULL) {
+            node_x_prev->next = node_y;
+        } else {
+            dummy->next = node_y;
+        }
+
+        if(node_y_prev != NULL) {
+            node_y_prev->next = node_x;
+        } else {
+            dummy->next = node_x;
+        }
+
+        ListNode *temp = node_y->next;
+        node_y->next = node_x->next;
+        node_x->next = temp;
+
+        head = dummy->next;
+        return head;
+    }
+
+    */
+
+
+    ListNode* swapNodes(ListNode* head, int k) 
+{
+    ListNode *dummy = new ListNode(-1, head);
+    ListNode *first_prev = dummy, *second_prev = dummy;
+    
+    // Find the (k-1)th node (first_prev)
+    for(int i = 1; i < k; i++) {
+        first_prev = first_prev->next;
+    }
+    
+    ListNode* first = first_prev->next;
+    ListNode* fast = first;
+    
+    // Move `fast` to the end, and `second_prev` will reach the (n-k)th node
+    while(fast->next) {
+        second_prev = second_prev->next;
+        fast = fast->next;
+    }
+    
+    ListNode* second = second_prev->next;
+
+    // If the two nodes to be swapped are the same node, no need to swap
+    if (first == second) return head;
+
+    // Swap the nodes by links
+    first_prev->next = second;
+    second_prev->next = first;
+
+    // Swap the next pointers of the nodes
+    ListNode* temp = first->next;
+    first->next = second->next;
+    second->next = temp;
+
+    return dummy->next;
+}
+
+};
