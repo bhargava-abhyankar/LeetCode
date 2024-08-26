15b1c5a42828c1c416fcccc201472e730558d782
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-08-26 06:25:36 +0530

    Time: 108 ms (78.99%) | Memory: 59.2 MB (96.66%) - LeetSync

--- 1582-design-browser-history/design-browser-history.cpp
+++ 1582-design-browser-history/design-browser-history.cpp
@@ -1,3 +1,6 @@
+
+/* Method 2:
+
 class MyListNode {
 public:
     string val;
@@ -52,6 +55,52 @@ public:
         return current->val;
     }
 };
+
+*/
+
+/* Method 3*/
+
+class BrowserHistory {
+public:
+    vector<string> history;
+    int current;
+    int last;
+
+    BrowserHistory(string homepage) 
+    {
+        history.push_back(homepage);
+        current = 0;
+        last = 0;
+    }
+    
+    void visit(string url) 
+    {
+        current = current + 1;
+
+        if(current < (history.size())) {
+            history[current] = url;
+
+        } else {
+            history.push_back(url);
+        }
+
+        last = current;
+    }
+    
+    string back(int steps) 
+    {
+        (current - steps) > 0 ? (current = (current - steps)):(current = 0);
+        return history[current];
+    }
+    
+    string forward(int steps) 
+    {
+        (current + steps) < last ? (current = current + steps): (current = last);
+        return history[current];
+    }
+};
+
+
 
 /**
  * Your BrowserHistory object will be instantiated and called as such:
