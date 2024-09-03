b0c2074c151d46bbc6f2b2cd9effa906e6b27164
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-09-03 19:41:40 +0530

    Time: 7 ms (71.36%) | Memory: 20.1 MB (5.40%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -12,6 +12,8 @@
 class Solution {
 public:
 
+    /*method 1: 
+
     bool check_if_sum_present(TreeNode *cur, int val, int &target)
     {
         if(cur == NULL)
@@ -33,4 +35,32 @@ public:
     {
         return check_if_sum_present(root, 0, targetSum);
     }
+
+    */
+
+    void check_if_sum_present(TreeNode *cur, int val, int &target, bool &ans)
+    {
+        if(cur == NULL)
+            return;
+        
+        val = val + cur->val;
+
+        if(cur->left == NULL && cur->right == NULL) {
+            if(val == target)
+                ans = true;
+        }
+
+        if(!ans)
+            check_if_sum_present(cur->left, val, target, ans);
+        if(!ans)
+            check_if_sum_present(cur->right, val, target, ans);
+    } 
+
+    bool hasPathSum(TreeNode* root, int targetSum) 
+    {
+        bool ans = false;
+        check_if_sum_present(root, 0, targetSum, ans);
+        return ans;
+    }
+
 };
