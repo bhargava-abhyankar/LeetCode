3bd88ad9d7cf2b885bb56668a6affb58dce73e76
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-09-03 06:12:31 +0530

    Time: 27 ms (54.12%) | Memory: 33.9 MB (27.34%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/merge-two-binary-trees.cpp
@@ -12,6 +12,8 @@
 class Solution {
 public:
 
+    /* method 1: My method 
+
     TreeNode* merge(TreeNode *root1_parent, TreeNode *root2_parent, TreeNode* root1, TreeNode* root2)
     {
         if(root1 == NULL && root2 == NULL) {
@@ -42,6 +44,61 @@ public:
             return root1;
 
         merge(NULL, NULL, root1, root2);
+        return root1;
+    }
+
+    */
+
+    /* Method 2: Using Iteration */
+
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) 
+    {
+        stack<pair<TreeNode*, TreeNode*>> st1; //node, parent
+        stack<pair<TreeNode*, TreeNode*>> st2;
+
+        if(root1 == NULL)
+            return root2;
+        if(root2 == NULL)
+            return root1;
+
+        st1.push({root1, NULL});
+        st2.push({root2, NULL});
+
+        while(!st2.empty()) {
+            TreeNode *p = st1.top().first;
+            TreeNode *p_parant = st1.top().second;
+            TreeNode *q = st2.top().first;
+            TreeNode *q_parent = st2.top().second;
+            st1.pop();
+            st2.pop();
+
+            p->val = p->val + q->val;
+
+            if(p->left && q->left) {
+                st1.push({p->left, p});
+                st2.push({q->left, q});
+            }
+
+            if(p->right && q->right) {
+                st1.push({p->right, p});
+                st2.push({q->right, q});
+            }
+
+            if(p->left == NULL && q->left) {
+                TreeNode *new_node = new TreeNode();
+                p->left = new_node;
+                st1.push({p->left, p});
+                st2.push({q->left, q});
+            }
+
+            if(p->right == NULL && q->right) {
+                TreeNode *new_node = new TreeNode();
+                p->right = new_node;
+                st1.push({p->right, p});
+                st2.push({q->right, q});
+            }
+        }
+
         return root1;
     }
 };
