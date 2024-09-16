4f677800ddc1f8a819d57e275706e506fd0db946
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-09-16 13:49:51 +0530

    Time: 15 ms (17.22%) | Memory: 21.1 MB (99.99%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -34,7 +34,7 @@ public:
 
     */
 
-    /* Method 2: */
+    /* Method 2: Always Choose right Middle Node as a Root 
 
     TreeNode* construct_bst(vector<int>& nums, int start, int end)
     {
@@ -55,6 +55,55 @@ public:
     TreeNode* sortedArrayToBST(vector<int>& nums) 
     {
         TreeNode *root = construct_bst(nums, 0, nums.size()-1);
+        return root;
+    }
+
+    */
+
+    /* Method 3: Using iteration */
+
+    struct NodeInfo {
+        TreeNode *parent;
+        int start;
+        int end;
+        bool isLeft;       //flag to know if current node belong to left of parent or right
+    };
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) 
+    {
+        if(nums.size() <= 0)
+            return NULL;
+        
+        stack<NodeInfo> st;
+        int start = 0, end = nums.size()-1;
+        int mid = (start + end) / 2;
+
+        TreeNode *root = new TreeNode(nums[mid]);
+        st.push({root, 0, mid - 1, true});
+        st.push({root, mid + 1, end, false});
+
+        while(!st.empty()) {
+            struct NodeInfo cur_nodeinfo = st.top(); 
+            st.pop();
+            start = cur_nodeinfo.start;
+            end = cur_nodeinfo.end;
+            mid = (start + end)/2;
+
+            if(start > end)
+                continue;
+            
+            TreeNode *new_node = new TreeNode(nums[mid]);
+
+            if(cur_nodeinfo.isLeft) {
+                cur_nodeinfo.parent->left = new_node;
+            } else {
+                cur_nodeinfo.parent->right = new_node;
+            }
+
+            st.push({new_node, start, mid-1, true});
+            st.push({new_node, mid+1, end, false});
+        }
+
         return root;
     }
 };
