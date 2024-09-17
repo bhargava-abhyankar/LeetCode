734f350a30a555e4723e7c6ec20960126bd3402a
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-09-17 21:50:23 +0530

    Time: 3 ms (83.02%) | Memory: 15.2 MB (30.18%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -12,6 +12,8 @@
 class Solution {
 public:
 
+    /* method 1: Using pre order recursion as its top down approach 
+
     void fill_level(TreeNode *cur, int level, vector<vector<int>> &ans)
     {
         if(cur == NULL)
@@ -33,6 +35,41 @@ public:
     {
         vector<vector<int>> ans;
         fill_level(root, 0, ans);
+        return ans;
+    }
+
+    */
+
+    vector<vector<int>> levelOrder(TreeNode* root) 
+    {
+        vector<vector<int>> ans;
+
+        if(root == NULL)
+            return ans;
+
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+
+            for(int i = 0; i < size; i++) {
+                TreeNode *cur = q.front();
+                q.pop();
+                level.push_back(cur->val);
+
+                if(cur->left) {
+                    q.push(cur->left);
+                }
+
+                if(cur->right) {
+                    q.push(cur->right);
+                }
+            }
+            ans.push_back(level);
+        }
+
         return ans;
     }
 };
