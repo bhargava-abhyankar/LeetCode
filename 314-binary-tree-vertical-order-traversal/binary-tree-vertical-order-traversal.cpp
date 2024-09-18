eaef45a99e7aa39a21d4069dc2a11eb6a0cd073f
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-09-18 23:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (76.28%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.cpp
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.cpp
@@ -12,6 +12,8 @@
 class Solution {
 public:
 
+    /* Method 1: BFS and Hash with sorting 
+
     vector<vector<int>> verticalOrder(TreeNode* root) 
     {
         vector<vector<int>> ans;
@@ -52,6 +54,52 @@ public:
         sort(columns.begin(), columns.end());
 
         for(auto i : columns) {
+            ans.push_back(hash[i]);
+        }
+
+        return ans;
+    }
+
+    */
+
+    /* Method 2: Using BFS and Hash, without sorting */
+
+    vector<vector<int>> verticalOrder(TreeNode* root) 
+    {
+        vector<vector<int>> ans;
+
+        if(root == NULL)
+            return ans;
+        
+        int start = INT_MAX, end = INT_MIN;
+        queue<pair<TreeNode *, int>> q;
+        unordered_map<int, vector<int>> hash;
+
+        q.push({root, 0});
+
+        while(!q.empty()) {
+            int size = q.size();
+
+            for(int i = 0; i < size; i++) {
+                TreeNode *cur = q.front().first;
+                int index = q.front().second;
+                q.pop();
+
+                hash[index].push_back(cur->val);
+                start = min(start, index);
+                end = max(end, index);
+
+                if(cur->left) {
+                    q.push({cur->left, index - 1});
+                }
+
+                if(cur->right) {
+                    q.push({cur->right, index + 1});
+                }
+            }
+        }
+
+        for(int i = start; i <=end; i++) {
             ans.push_back(hash[i]);
         }
 
