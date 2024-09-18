a189a10a6b9afd2a0e79364c1ed4c1de9e530fd8
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-09-18 22:58:38 +0530

    Time: 3 ms (69.66%) | Memory: 15.2 MB (55.05%) - LeetSync

---
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    vector<vector<int>> verticalOrder(TreeNode* root) 
+    {
+        vector<vector<int>> ans;
+
+        if(root == NULL)
+            return ans;
+        
+        queue<pair<TreeNode*, int>> q;
+        unordered_map<int, vector<int>> hash;     // if we use map, no need to sort. but every search takes time
+
+        q.push({root, 0});
+
+        while(!q.empty()) {
+            int size = q.size();
+
+            for(int i = 0; i < size; i++) {
+                TreeNode *cur = q.front().first;
+                int index = q.front().second;
+                q.pop();
+
+                hash[index].push_back(cur->val);
+
+                if(cur->left) {
+                    q.push({cur->left, index - 1});
+                }
+                if(cur->right) {
+                    q.push({cur->right, index + 1});
+                }
+            }
+        }
+
+        vector<int> columns;
+
+        for(const auto &pair: hash) {
+            columns.push_back(pair.first);
+        }
+
+        sort(columns.begin(), columns.end());
+
+        for(auto i : columns) {
+            ans.push_back(hash[i]);
+        }
+
+        return ans;
+    }
+};
