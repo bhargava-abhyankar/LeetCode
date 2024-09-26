03e60258884c09705e843c4aa1272170e9a49504
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-09-26 12:49:12 +0530

    Time: 8 ms (49.96%) | Memory: 20 MB (5.01%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -11,6 +11,9 @@
  */
 class Solution {
 public:
+
+    /* Method 1: Using indexing and BFS 
+
     int widthOfBinaryTree(TreeNode* root) 
     {
         long long ans = 0;
@@ -40,6 +43,39 @@ public:
             }
         }
 
+        return ans;
+    }
+
+    */
+
+    /* Method 2: Using DFS, pre order level traversal. 
+       idea is to store the leftmost or first index of each level. then for all the visits of the nodes,
+       node index - first index i.e should be taken from the vector + 1 is compared with existing ans
+       variable. 
+     */
+
+    void dfs(TreeNode* cur, int level, unsigned long long index, 
+             vector<unsigned long long> &level_first_index, unsigned long long &ans)
+    {
+        if(cur == NULL)
+            return;
+        
+        if(level_first_index.size() == level) {
+            level_first_index.push_back(index);
+        }
+
+        ans = max(ans, (index - level_first_index[level] + 1));
+
+        dfs(cur->left, level + 1, 2*index +1, level_first_index, ans);
+        dfs(cur->right, level + 1, 2*index +2, level_first_index, ans);
+    }
+
+    int widthOfBinaryTree(TreeNode* root) 
+    {
+        unsigned long long ans = 0;
+        vector<unsigned long long> level_first_index; // first(leftmost) nodes at each level
+
+        dfs(root, 0, 0, level_first_index, ans);
         return ans;
     }
 };
