7683588c7a9eabc0e1b973fd8eba7aaa3a72896b
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-09-27 15:23:15 +0530

    Time: 740 ms (5.07%) | Memory: 188.5 MB (5.16%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.cpp
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -11,6 +11,9 @@
  */
 class Solution {
 public:
+
+    /* Method 1: Using BFS
+
     bool isEvenOddTree(TreeNode* root) 
     {
         int level = 0;
@@ -58,4 +61,43 @@ public:
         }
         return true;
     }
+
+    */
+
+    void check_condition(int val, int level, vector<vector<int>> &nodes, bool &ans)
+    {
+        int size = nodes[level].size();
+        if(level % 2 == 0) {
+            if(val % 2 == 0 || (size > 0 && nodes[level][size-1] >= val))
+                ans = false;
+        } else {
+            if(val % 2 == 1 || (size > 0 && nodes[level][size-1] <= val))
+                ans = false;
+        }
+    }
+
+    void checkifevenoddtree(TreeNode* cur, int level, vector<vector<int>> &nodes, bool &ans)
+    {
+        if(cur == NULL || !ans)
+            return;
+
+        if(level == nodes.size()) {            
+            nodes.push_back(vector<int>());
+        } 
+
+        check_condition(cur->val, level, nodes, ans);
+        nodes[level].push_back(cur->val);
+
+        checkifevenoddtree(cur->left, level + 1, nodes, ans);
+        checkifevenoddtree(cur->right, level + 1, nodes, ans);    
+    }
+
+    bool isEvenOddTree(TreeNode* root) 
+    {
+        bool ans = true;
+        vector<vector<int>> nodes;
+        checkifevenoddtree(root, 0, nodes, ans);
+        return ans;
+    }
+
 };
