c55370e4bc7c42190ef9bd48829d96e55bb6f5d4
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-09-29 19:36:21 +0530

    Time: 87 ms (88.23%) | Memory: 85.1 MB (21.29%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -12,6 +12,8 @@
 class Solution {
 public:
 
+    /* Method 1: Using pre order recursion 
+
     void count_good_nodes(TreeNode* cur, int max_till_now, int& count)
     {
         if(cur == NULL)
@@ -32,4 +34,41 @@ public:
         count_good_nodes(root, INT_MIN, count);
         return count;
     }
+
+    */
+
+    /* Method 2: Using pre order iteration */
+
+    int goodNodes(TreeNode* root) 
+    {
+        if(root == NULL)
+            return 0;
+
+        int count = 0;
+        stack<pair<TreeNode *, int>> st;
+        st.push({root, INT_MIN});
+
+        while(!st.empty()) {
+            TreeNode *current = st.top().first;
+            int max_till_now = st.top().second;
+            st.pop();
+
+            if(current->val >= max_till_now) {
+                count++;
+                max_till_now = current->val;
+            }
+
+            if(current->right) {
+                st.push({current->right, max_till_now});
+            }
+
+            if(current->left) {
+                st.push({current->left, max_till_now});
+            }
+
+        }
+
+        return count;
+    }
+
 };
