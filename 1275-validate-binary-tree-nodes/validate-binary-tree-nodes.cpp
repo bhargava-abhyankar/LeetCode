08b1b9eef8649e5d8019e54e1208bc815b549190
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-10-14 17:40:34 +0530

    Time: 47 ms (48.24%) | Memory: 43.5 MB (43.32%) - LeetSync

--- 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -72,7 +72,7 @@ public:
 
     */
 
-    /* Method 2: Using dfs iteration */
+    /* Method 2: Using dfs iteration 
 
     int find_root(int n, vector<int>& leftChild, vector<int>& rightChild)
     {
@@ -139,6 +139,77 @@ public:
                 return false;
         }
         
+        return true;
+    }
+
+    */
+
+    int find_root(int n, vector<int>& leftChild, vector<int>& rightChild)
+    {
+        unordered_set<int> hash;
+
+        for(int i = 0; i < n; i++) {
+            if(leftChild[i] != -1)
+                hash.insert(leftChild[i]);
+            if(rightChild[i] != -1)
+                hash.insert(rightChild[i]);
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(hash.find(i) == hash.end())
+                return i;
+        }
+
+        return -1;
+    }
+
+    bool bfs(int root, int n, vector<int>& leftChild, vector<int>& rightChild, vector<bool> &is_visited)
+    {
+        queue<int> q;
+        q.push(root);
+        is_visited[root] = true;
+
+        while(!q.empty()) {
+            int cur = q.front();
+            q.pop();
+
+            int x = leftChild[cur], y = rightChild[cur];
+
+            if ((x != -1 && is_visited[x] == true) || (y != -1 && is_visited[y] == true)) {
+                return true;
+            }
+
+            if(x != -1) {
+                is_visited[x] = true;
+                q.push(x);
+            }
+
+            if(y != -1) {
+                is_visited[y] = true;
+                q.push(y);
+            }
+        }
+
+        return false;
+    }
+
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) 
+    {
+        int root = find_root(n, leftChild, rightChild);
+
+        if(root == -1)
+            return false;
+        
+        vector<bool> is_visited(n, false);
+
+        if(bfs(root, n, leftChild, rightChild, is_visited))
+            return false;
+        
+        for(int i = 0; i < n; i++) {
+            if(is_visited[i] == false)
+                return false;
+        }
+
         return true;
     }
 
