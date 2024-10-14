2ebf867766330b8543270f55556c8a2fb1411068
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-10-14 17:04:26 +0530

    Time: 60 ms (32.79%) | Memory: 43 MB (44.03%) - LeetSync

--- 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -8,7 +8,8 @@ public:
       4. There should not be a cycle 
     */
 
-    /* Method 1: Using DFS */
+
+    /* Method 1: Using DFS
     
     int find_root(int n, vector<int>& leftChild, vector<int>& rightChild)
     {
@@ -27,7 +28,6 @@ public:
 
         return -1;
     }
-
 
     bool dfs(int i, int parent, vector<int>& leftChild, vector<int>& rightChild, vector<bool> &is_visited)
     {
@@ -67,6 +67,78 @@ public:
                 return false;
         }
 
+        return true;
+    }
+
+    */
+
+    /* Method 2: Using dfs iteration */
+
+    int find_root(int n, vector<int>& leftChild, vector<int>& rightChild)
+    {
+        unordered_set<int> hash;
+
+        for(int i = 0; i < n; i++) {
+            if(leftChild[i] != -1)
+                hash.insert(leftChild[i]);
+            if(rightChild[i] != -1)
+                hash.insert(rightChild[i]);
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(hash.find(i) == hash.end())
+                return i;
+        }
+
+        return -1;
+    }
+
+    bool dfs(int root, int n, vector<int>& leftChild, vector<int>& rightChild, vector<bool> &is_visited)
+    {
+        stack<int> st;
+        st.push(root);
+        is_visited[root] = true;
+
+        while(!st.empty()) {
+            int cur = st.top();
+            st.pop();
+
+            int x = leftChild[cur], y = rightChild[cur];
+
+            if((x != -1 && is_visited[x] == true) || (y != -1 && is_visited[y] == true))
+                return true;
+            
+            if(y != -1) {
+                st.push(y);
+                is_visited[y] = true;
+            }
+                
+            if(x != -1) {
+                st.push(x);
+                is_visited[x] = true;
+            }
+        }
+
+        return false;
+    }
+
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) 
+    {
+        int root = find_root(n, leftChild, rightChild);
+
+        if(root == -1)
+            return false;
+        
+        vector<bool> is_visited(n, false);
+
+        if(dfs(root, n, leftChild, rightChild, is_visited))
+            return false;
+        
+        for(int i = 0; i < n; i++) {
+            if(is_visited[i] == false)
+                return false;
+        }
+        
         return true;
     }
 
