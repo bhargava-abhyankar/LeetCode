dc4f2d213514d612218789aa8cb13419a363d2d0
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-10-17 22:35:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (20.83%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
@@ -11,6 +11,23 @@
  */
 class Solution {
 public:
+
+    /* Method 1: Using recursion */
+
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) 
+    {
+        if(root1 == root2) 
+            return true;
+        if(root1 == NULL || root2 == NULL || root1->val != root2->val)
+            return false;
+        
+        return ((flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right)) || 
+                (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left)));
+    }
+
+
+    /* Method 2: Using BFS. This is optimized solution with O(n) time complexity.
+
     bool flipEquiv(TreeNode* root1, TreeNode* root2) 
     {
         if(root1 == root2)
@@ -59,4 +76,6 @@ public:
 
         return true;
     }
+
+    */
 };
