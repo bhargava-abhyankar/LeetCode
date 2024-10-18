bf74e423677bf710590a13818abf6830c56ee47a
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-10-18 16:29:01 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (12.54%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
 
-    /* Sub optimal method first to do */
+    /* Sub optimal method first to do 
 
     int countNodes(TreeNode* root) 
     {
@@ -24,5 +24,61 @@ public:
         return(left + right + 1);
     }
 
+    */
+
+    int find_depth(TreeNode *cur)
+    {
+        int depth = 0;
+        
+        while(cur->left) {
+            depth = depth + 1;
+            cur = cur->left;
+        }
+
+        return depth;
+    }
+
+    bool exists(TreeNode* cur, int d, int index)
+    {
+        int start = 0, end = pow(2, d) - 1;
+
+        for(int i = 0; i < d; i++) {
+            int peviot = (start + end) / 2;
+
+            if(index <= peviot) {
+                cur = cur->left;
+                end = peviot;
+            } else {
+                cur = cur->right;
+                start = peviot + 1;
+            }
+        }
+
+        return cur != NULL;
+    }
+
+    int countNodes(TreeNode* root) 
+    {
+        if(root == NULL)
+            return 0;
+        
+        int depth = find_depth(root);
+
+        if(depth == 0)
+            return 1;
+        
+        int start = 1, end = pow(2, depth) - 1;
     
+        while(start <= end) {
+            int piviot = (end + start) / 2;
+
+            if(exists(root, depth, piviot))
+                start = piviot + 1;
+            else 
+                end = piviot - 1;
+        }
+
+        return pow(2, depth) - 1 + start;
+    }
+
 };
