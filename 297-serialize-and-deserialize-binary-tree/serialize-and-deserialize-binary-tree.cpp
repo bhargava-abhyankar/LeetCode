6302c13f36c7cd805a721fe5deb9f88d36876ca3
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-10-21 19:26:35 +0530

    Time: 25 ms (76.99%) | Memory: 30.7 MB (53.05%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -10,6 +10,8 @@
 class Codec {
 public:
 
+    /* Method 1: Using Pre order recursion 
+
     void recursive_serialize(TreeNode* cur, ostringstream &ans)
     {
         if(cur == NULL) {
@@ -51,6 +53,80 @@ public:
         return root;
     }
 
+    */
+
+
+    /* Method 2: Using BFS */
+
+    string serialize(TreeNode* root) 
+    {
+        ostringstream ans;
+
+        if(root == NULL)
+            return ans.str();
+        
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            int size = q.size();
+
+            for(int i = 0; i < size; i++) {
+                TreeNode *cur = q.front();
+                q.pop();
+
+                if(cur == NULL) {
+                    ans << "null,";
+                } else {
+                    ans << cur->val << ",";
+
+                    q.push(cur->left);
+                    q.push(cur->right);
+                }
+            }
+        }
+
+        return ans.str();
+    }
+
+
+    TreeNode* deserialize(string data) 
+    {
+        string node;
+
+        if(data == "")
+            return NULL;
+    
+        istringstream input(data);
+        queue<TreeNode *> q;
+
+        getline(input, node, ',');
+        TreeNode *root = new TreeNode(stoi(node));
+        q.push(root);
+
+        while(!q.empty()) {
+            string left_child;
+            string right_child;
+
+            getline(input, left_child, ',');
+            getline(input, right_child, ',');
+
+            TreeNode *parent = q.front();
+            q.pop();
+
+            if(left_child != "null") {
+                parent->left = new TreeNode(stoi(left_child));
+                q.push(parent->left);
+            }
+
+            if(right_child != "null") {
+                parent->right = new TreeNode(stoi(right_child));
+                q.push(parent->right);
+            }
+        }
+
+        return root;
+    }
 
 };
 
