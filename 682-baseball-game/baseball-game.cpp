c8e8f48a0408fab86c32510311ac66106d6e8b84
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-10-22 23:43:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (98.43%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
 
+    /* Method 1: Using Stack 
+
     int calPoints(vector<string>& operations) 
     {
         int ans = 0;
@@ -32,6 +34,40 @@ public:
         while(!st.empty()) {
             ans = ans + st.top();
             st.pop();
+        }
+
+        return ans;
+    }
+    */
+
+    /* Method 2: Using Array or vector */
+
+    int calPoints(vector<string>& operations) 
+    {
+        int n = operations.size(), ans = 0, cur_index = -1;
+        int record[n];
+        memset(record, 0, n);
+
+        for(int i = 0; i < n; i++) {
+            if(operations[i] == "C") {
+                ans = ans - record[cur_index];
+                cur_index--;
+            } else if (operations[i] == "D") {
+                record[cur_index + 1] = record[cur_index] * 2;
+                ans = ans + record[cur_index + 1];
+                cur_index++;
+            } else if (operations[i] == "+") {
+                int a = record[cur_index];
+                int b = record[cur_index - 1];
+                record[cur_index + 1] = a + b;
+                ans = ans + record[cur_index + 1];
+                cur_index++;
+            } else {
+                cur_index++;
+                int score = stoi(operations[i]);
+                record[cur_index] = score; 
+                ans = ans + record[cur_index];
+            }    
         }
 
         return ans;
