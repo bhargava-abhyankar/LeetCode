00614092d0f70bc8107b8b4ba723e2bffdfc69b7
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-10-29 12:15:56 +0530

    Time: 7 ms (16.80%) | Memory: 12.1 MB (5.20%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    /* Method 1: Simple Brute force */
+    /* Method 1: Simple Brute force
 
     string makeGood(string s) 
     {
@@ -24,12 +24,25 @@ public:
         return s; 
     }
 
-    /* Method 2: Using Recursion 
+    */
+
+    /* Method 2: Using Recursion */
 
     string makeGood(string s)
     {
-       
+        if(s.length() == 0)
+            return s;
+        
+        for(int i = 0; i < s.length()-1; i++) {
+
+            if(abs(s[i] - s[i + 1]) == 32) {
+                s = s.substr(0, i) + s.substr(i+2);
+                return makeGood(s);
+            }
+        }
+
+        return s;
     }
 
-    */
+    
 };
