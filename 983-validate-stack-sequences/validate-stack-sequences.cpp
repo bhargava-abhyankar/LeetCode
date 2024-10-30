1c558848b302cc6c0ccf31abca9d1c798218ad20
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-10-30 16:23:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (97.03%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.cpp
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    /* Method 1: Using stack */
+    /* Method 1: Using stack 
 
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) 
     {
@@ -19,4 +19,26 @@ public:
 
         return j == popped.size();    
     }
+
+    */
+
+    /* Method 2: Two Pointer. Use given array pushed as stack */
+
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) 
+    {
+        int i = 0, j = 0;
+
+        for(int k = 0; k < pushed.size(); k++) {
+            pushed[i] = pushed[k];
+            i++;
+
+            while(i > 0 && pushed[i-1] == popped[j]) {
+                j++;
+                i--;
+            }
+        }
+
+        return i == 0;
+    }
+
 };
