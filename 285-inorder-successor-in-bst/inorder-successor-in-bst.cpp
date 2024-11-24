58eaa7d4efff4f552b3619861d2108ab5543e822
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2024-11-24 22:58:23 +0530

    Time: 24 ms (48.94%) | Memory: 21.5 MB (71.78%) - LeetSync

--- 285-inorder-successor-in-bst/inorder-successor-in-bst.cpp
+++ 285-inorder-successor-in-bst/inorder-successor-in-bst.cpp
@@ -10,6 +10,9 @@
 class Solution {
 public:
 
+
+    /* Method 1: Inorder succesor of binary tree 
+
     void find_inorder_successor(TreeNode* cur, TreeNode* p, TreeNode *&ans, bool &is_found)
     {
         if(cur == NULL)
@@ -35,4 +38,26 @@ public:
         find_inorder_successor(root, p, ans, is_found);
         return ans;
     }
+
+    */
+
+    /* Method 2: Inorder succesor of binary search tree */
+
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) 
+    {
+        TreeNode *ans = NULL, *cur = root;
+
+        while(cur) {
+            if(cur->val <= p->val) {
+                cur = cur->right;
+            } else {
+                ans = cur;
+                cur = cur->left;
+            }
+        }
+
+        return ans;
+    }
+
+
 };
