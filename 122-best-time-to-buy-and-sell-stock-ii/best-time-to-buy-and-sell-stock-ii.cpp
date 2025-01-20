fa0de5032d418d693445a62144473dd3802b452a
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2025-01-20 16:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (73.55%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    /*
     int maxProfit(vector<int>& prices) 
     {
         
@@ -14,6 +16,20 @@ public:
                 min_price =  prices[i];
             }
         }
+        return max_profit;
+    }
+
+    */
+
+    int maxProfit(vector<int>& prices) 
+    {
+        int max_profit = 0;
+
+        for(int i = 1; i < prices.size(); i++) {
+            if(prices[i] > prices[i-1])
+                max_profit = max_profit + prices[i] - prices[i-1];
+        }
+
         return max_profit;
     }
 };
