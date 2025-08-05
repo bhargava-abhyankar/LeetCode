6fc1ecca558c9638b6e44b226c8c5448712b0ee1
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2025-08-05 05:40:17 +0530

    Time: 0 ms (100.00%) | Memory: 113.6 MB (94.15%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
 
-    /*Method 1: Cumulative Sum */
+    /* Method 1: Brute force 
+
+    double findMaxAverage(vector<int>& nums, int k) 
+    {
+        double ans = INT_MIN;
+
+        for(int i = 0; i <= nums.size()-k; i++) {
+            double sum = 0;
+
+            for(int j = i; j < i + k; j++) {
+                sum = sum + nums[j];
+            }
+
+            ans = max(ans, sum/k);
+        }
+
+        return ans;
+    }
+
+    */
+
+    /* Method 2: Cumulative Sum 
 
     double findMaxAverage(vector<int>& nums, int k) 
     {
@@ -20,4 +41,29 @@ public:
 
         return ans;
     }
+
+    */
+
+    /* Method 3: Sliding Window */
+
+    double findMaxAverage(vector<int>& nums, int k) 
+    {
+        double sum = 0;
+
+        for(int i = 0; i < k; i++) {
+            sum = sum + nums[i];
+        }
+
+        double max_sum = sum;
+
+        for(int i = k; i < nums.size(); i++) {
+            sum =  sum + nums[i] - nums[i-k];
+
+            if(sum > max_sum)
+                max_sum = sum;
+        }
+
+        return(max_sum/k);
+    }
+
 };
