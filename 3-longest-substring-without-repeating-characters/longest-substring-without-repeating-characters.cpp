b8cad5126720376958bc31bf7bbe230be4dfe6aa
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2025-08-05 06:05:08 +0530

    Time: 20 ms (25.43%) | Memory: 14.3 MB (22.70%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+
+    /*
+
     int lengthOfLongestSubstring(string s) 
     {
         int ans = 0, i = 0, j = 0;
@@ -14,6 +17,7 @@ public:
             }
 
             ans = max(ans, j - i);
+
             if(j < s.length()) {
                 
                 auto itr = hash.find(s[i]);
@@ -22,8 +26,32 @@ public:
                     hash.erase(itr);
                 }
             }
+
             i++;
         }
         return ans;
+    }
+
+    */
+
+    /* Method 1: Sliding window */
+
+    int lengthOfLongestSubstring(string s) 
+    {
+        int max_length = 0;
+        unordered_set<char> hash;
+        
+        int start = 0;
+
+        for(int end = 0; end < s.size(); end++) {
+            while(hash.find(s[end]) != hash.end()) {
+                hash.erase(s[start]);
+                start++;
+            }
+            hash.insert(s[end]);
+            max_length = max(max_length, end - start + 1);
+        }
+
+        return max_length;
     }
 };
