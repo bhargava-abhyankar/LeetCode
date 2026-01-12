a9bb16f8054fc3fa3d057f5fb9b5b98ad7a862a2
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2026-01-12 06:52:36 -0800

    Time: 0 ms (100.00%) | Memory: 13.6 MB (75.69%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -36,7 +36,7 @@ public:
     }
     */
 
-    /* lower bound approach */
+    /* lower bound approach 
 
     int searchInsert(vector<int>& nums, int target) 
     {
@@ -61,4 +61,25 @@ public:
         return ans;
     }
 
+    */
+
+
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        int start = 0, end = nums.size()-1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(target == nums[mid]) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid -1;
+            }
+        }
+
+        return start;
+    }
 };
