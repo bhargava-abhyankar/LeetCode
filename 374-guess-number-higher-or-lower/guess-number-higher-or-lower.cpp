c14f5e83b2bb14cf5cf1c8325a93af22987f5c8b
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2026-01-12 07:06:52 -0800

    Time: 0 ms (100.00%) | Memory: 8 MB (40.72%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -9,6 +9,8 @@
 
 class Solution {
 public:
+
+    /*
     int guessNumber(int n) 
     {
         int start = 1, end = n;
@@ -25,6 +27,28 @@ public:
                 end = mid - 1;
             }
         }
+        return -1;
+    }
+
+    */
+
+
+    int guessNumber(int n) 
+    {
+        int start = 1, end = n;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(guess(mid) == 0) {
+                return mid;
+            } else if (guess(mid) == -1) {
+                end = mid - 1;
+            } else if (guess(mid) == 1) {
+                start = mid + 1;
+            }
+        }
+
         return -1;
     }
 };
