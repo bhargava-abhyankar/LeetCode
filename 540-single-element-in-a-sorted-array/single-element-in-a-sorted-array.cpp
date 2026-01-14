aaf39cbdb50df5203f5a19076c07484f00b25563
bhargava-abhyankar <162902289+bhargava-abhyankar@users.noreply.github.com>
2026-01-14 09:49:40 -0800

    Time: 0 ms (100.00%) | Memory: 26.2 MB (66.94%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    /*
     int singleNonDuplicate(vector<int>& nums) 
     {
         int start = 0, end = nums.size() - 1;
@@ -17,6 +19,8 @@ public:
         return nums[start];
     }
 
+    */
+
     /* Logic explanation.
        This can be solved by leanear search or by xor the every element with the previos result. at last result will have single element. this takes o(n).
        To solve this in o(log n), use binary search. Idea/goal is to devide the array and find in which half the unique element is present. So we search
@@ -32,4 +36,15 @@ public:
 	   so, update end to mid. At last return the nums[left]
 
      */
+
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        for(int i = 0; i < nums.size()-1; i = i+2) {
+            if(nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+
+        return nums.back();
+    }
 };
